cf6a7f26f8196d084d70a98883f68f43696cd2dd
Kenan Kurda <intecbrussel24@gmail.com>
2024-02-27 15:55:49 +0100

    Ok

--- LibraryNetFramework/Model/Book.cs
+++ LibraryNetFramework/Model/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace LibraryNetFramework.Model
     public class Book
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+        [Required]
+
         public string Author { get; set; }
         public decimal Price { get; set; }
         public string Describe { get; set; }
